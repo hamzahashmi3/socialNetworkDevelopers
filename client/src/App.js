f9af224d28d723e8bfb1bef00604aa210c8462c7
hamzahashmi3 <technocannons3@gmail.com>
2022-06-07 18:03:17 +0500

    register, login,landingpage, frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Layout/Navbar';
+import LandingPage from './components/Layout/LandingPage';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<LandingPage/>} />
+        </Routes>
+        
+          
+
+        <section className='container'>
+          <Routes>
+            <Route exact path='/login' element={<Login />}/>
+            <Route exact path='/register' element={<Register/>} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
