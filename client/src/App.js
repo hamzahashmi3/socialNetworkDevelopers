f969acbd80b9685da1f5a7c0512665b8eae4c144
hamzahashmi3 <technocannons3@gmail.com>
2022-06-15 17:54:22 +0500

    feat(login logout auth): login logout auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useEffect} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
@@ -10,8 +10,21 @@ import Alert from './components/Layout/Alert';
 // Redux
 import {Provider} from 'react-redux';
 import Store from './Store.js';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/Auth';
+import store from './Store.js';
+
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
 
 const App=()=> {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
   return (
     <Provider store={Store}>
       <Router>
